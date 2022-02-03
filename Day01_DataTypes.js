3252f1bbf28dc3ea139d35dc53f2bee468c9889f
Asim Gokkus <asimgokkus1@gmail.com>
2022-02-03 08:56:53 +0100

    first js commit

---
+++ Day01_DataTypes.js
@@ -0,0 +1,43 @@
+console.clear()
+/* Primitive Data Types
+
+string, number, boolean, null, undefined
+
+*/
+var myNum = 14;
+console.log(myNum)
+console.log(typeof myNum)
+
+var myNum = 14.356
+console.log(myNum)
+console.log(typeof myNum)
+
+var myNum= 20/0 
+console.log(myNum)
+console.log(typeof myNum)
+
+var myName = 'anadolu'
+console.log(myName)
+console.log(typeof myName)
+
+var num1 = 20+myName
+console.log(num1)//20anadolu
+console.log(typeof num1)//string
+
+var isExist = true
+console.log(isExist)//true
+console.log(typeof isExist)//boolean
+
+var isExist = false
+console.log(isExist)//false
+console.log(typeof isExist)//"boolean"
+
+
+var selectedCar
+console.log(selectedCar)
+console.log(typeof selectedCar)
+
+var pickedColor = null
+console.log(pickedColor)//null
+console.log(typeof pickedColor)//object
+
---
+++ Day01_Variables.js
@@ -0,0 +1,23 @@
+alert("alert works here")
+console.log("Hello World")
+var myName
+console.log(myName)
+//var variables can be declared multiple times
+myName = "Asim"
+console.log(myName)
+myName = "Mark"
+console.log("Hello World")
+var myName
+console.log(myName)
+//var variables can be declared multiple times
+myName = "Asim"
+console.log(myName)
+myName = "Mark"
+console.log(myName)
+console.log(typeof myName)
+myName=true;
+console.log(typeof myName)
+myName=55
+console.log(typeof myName)
+var fName = "Mehmet" , lName="Ayaz"
+console.log(fName+ ' '+ lName)
