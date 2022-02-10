8eba42afe3700abfe3c4d1e476e1c1d7e133d48e
Asim Gokkus <asimgokkus1@gmail.com>
2022-02-10 15:45:04 +0100

    OOP in JS

---
+++ Day07_EvenOddNumbers.js
@@ -0,0 +1,13 @@
+function isEven(){
+
+
+    let number = document.getElementById("TextBox").value;
+
+    if(number %2 === 0){
+        alert(number +" is an Even number");
+    }else{
+
+        alert(number +" is an Odd number");
+    }
+
+}
---
+++ ForPractice.js
@@ -0,0 +1,5 @@
+function sum (n1,n2){
+   return n1+n2+100;
+ }
+     
+  console.log(sum(2,3));
